c267a8cce52090ff9f3d828fd9065271be26b7a4
nanilev <dianuski99@gmail.com>
2019-03-29 12:19:18 +0000

    added unity project

---
+++ UnityTest/Assets/Scripts/Behave.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Behave : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("yolo");
+        transform.localScale = new Vector3(3,3,3);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 rot = new Vector3(0.3f, 0.3f, 0.3f);
+        transform.Rotate(rot);
+    }
+}
