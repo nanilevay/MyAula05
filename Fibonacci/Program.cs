2da4d12b727c5f6b8a01fa289bb2a2794a874ae4
nanilev <dianuski99@gmail.com>
2019-03-29 12:51:26 +0000

    added fibonacci sequence

---
+++ Fibonacci/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Fibonacci
+{
+    class Program
+    {
+        static int Fibonacci(int n)
+        {
+            int number;
+
+            if (n <= 2)
+                number = 1;
+            else
+                number = Fibonacci(n - 2) + Fibonacci(n - 1);
+
+            return number;
+        }
+
+        static void Main(string[] args)
+        {
+            int answer;
+         
+            Console.WriteLine("choose a number within the sequence");
+            answer = Fibonacci(Convert.ToInt32(Console.ReadLine()));
+            Console.WriteLine(answer);
+
+        }
+    }
+}
