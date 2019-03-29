29f7e72cadde4175fb32980a315a60f3cf7733c6
nanilev <dianuski99@gmail.com>
2019-03-29 10:42:40 +0000

    added countToN

--- MyFirstMethod/Program.cs
+++ MyFirstMethod/Program.cs
@@ -4,9 +4,27 @@ namespace MyFirstMethod
 {
     class Program
     {
+        static void CountToN(int n)
+        {
+            for(int i = 1; i <= n; i++)
+            {
+                Console.WriteLine(i);
+            }
+        }
+
+        static void CountTo10()
+        {
+            
+            CountToN(10);
+
+        }
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+         
+            CountToN(2);
+            CountToN(8);
+            CountTo10();
         }
     }
 }
