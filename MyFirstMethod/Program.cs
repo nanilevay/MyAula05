65e8e8f4bcbf3e161ec3c6e1e8335232e2d817f0
nanilev <dianuski99@gmail.com>
2019-03-29 11:45:07 +0000

    added documentation and overload

--- MyFirstMethod/Program.cs
+++ MyFirstMethod/Program.cs
@@ -4,6 +4,12 @@ namespace MyFirstMethod
 {
     class Program
     {
+   
+        /// <summary>
+        /// Count to n.
+        /// </summary>
+        /// <param name="n">Max number</param>
+        /// <returns>Int count from 1 to n</returns>
         static void CountToN(int n)
         {
             for(int i = 1; i <= n; i++)
@@ -12,17 +18,45 @@ namespace MyFirstMethod
             }
         }
 
+
+        static void CountToN(int n, int o)
+        {
+            if (n < o)
+            {
+            for(int i = n; i <= o; i++)
+            {
+                Console.WriteLine(i);
+            }
+            }
+
+            if (n > o)
+            {
+                for(int i = o; i <= n; i++)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+            else
+                Console.WriteLine(n);
+        }
+        
+        /// <summary>
+        /// Count to 10.
+        /// </summary>
+        /// <returns></returns>
+        
         static void CountTo10()
         {
             
             CountToN(10);
 
         }
-
+        
         static void Main(string[] args)
         {
          
             CountToN(2);
+            CountToN(2,4);
             CountToN(8);
             CountTo10();
         }
